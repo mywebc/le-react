2001c3ecf3c47bd4d813488cab14176bea3d572e
chenxiaole <2524729719@qq.com>
2020-09-01 14:31:45 +0800

    配置sass

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,11 +1,12 @@
 import React from "react"
+import "./Button.scss"
 
 interface IButton {
     txt: string;
 }
 
 const Button: React.FC<IButton> = (props) => {
-    return <div>{props.txt}</div>
+    return <div className="le-button">{props.txt}</div>
 }
 
 export default Button;
