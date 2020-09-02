61ae6e96c397b5b1dc81da0199946d36acb551dc
chenxiaolani <2524729719@qq.com>
2020-09-02 21:47:13 +0800

    去除ButtonGroup验证

--- src/components/button/ButtonGroup.tsx
+++ src/components/button/ButtonGroup.tsx
@@ -1,36 +1,11 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 import "./Button.scss"
-import { Console } from "console"
 
 const ButtonGroup: React.FC = (props) => {
-  const [isValid, setValid] = useState(false)
-
-  useEffect(() => {
-    // console.log(props)
-    // try {
-    //   (props.children as any).map((_: { type: { name: string } }) => {
-    //     if (_.type.name === "Button" || _.type.name === "ProxyFacade") {
-    //       setValid(true)
-    //     } else {
-    //       console.error("The child element is not valid!!")
-    //     }
-    //   })
-    // } catch (error) {
-    //   setValid(false)
-    //   console.error("The child element is not valid!!")
-    // }
-  }, [])
-
-  useEffect(() => {
-    if (Array.isArray(props.children) && props.children.length < 2) {
-      setValid(false)
-      console.error("Contains at least two child elements!!")
-    }
-  }, [])
 
   return (
     <div className="le_btn_group">
-      {isValid && props.children}
+      {props.children}
     </div>
   )
 }
