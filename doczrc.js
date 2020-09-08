9a634d1e4ca8b9b7c0ea332ab8c72464ad3643f0
chenxiaole <2524729719@qq.com>
2020-09-08 11:36:29 +0800

    updaTE

---
+++ doczrc.js
@@ -0,0 +1,17 @@
+export default {
+  typescript: true,
+  title: "React UI",
+  menu: [
+    'Getting Started',
+    // { name: 'Components', menu: ['Input','Button'] }
+    'General',
+    'Form'
+  ],
+  themeConfig: { 
+    colors: {
+      header: {
+        bg: 'green'
+      }
+    },
+  } 
+}
