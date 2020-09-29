3949cde8bc2f0ac410ecabe15e197594330e8352
chenxiaole <2524729719@qq.com>
2020-09-29 09:52:33 +0800

    drawer develop

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,35 @@
 import React, { useState } from 'react';
 import Button from "./components/button/Button";
-import ButtonGroup from "./components/button/ButtonGroup"
-import Message from "./components/message/Message"
-import Rate from "./components/rate/Rate"
 import "./styles/index.scss"
 import "./App.scss"
 import Icon from "./components/icon/Icon"
 import Modal from "./components/modal/Modal"
 import Notification from "./components/notification/Notification"
 import Affix from "./components/affix/Affix"
+import Drawer from "./components/drawer/Drawer"
 
 type notificationType = "info" | "success" | "error" | "warning" | "open"
 
 const App = () => {
   const [visible, setVisible] = useState<boolean>(false);
 
-  const handleClick = (type: notificationType) => {
-    Notification[type]({
-      message: 'Notification Title',
-      description: 'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
-      duration: 0,
-      // placement: "topRight",
-      // icon: "smile"
-    })
-  }
-
-  const modalClick = () => {
-    setVisible(true)
-    console.log("app", visible)
-  }
-
-  const handleModalClick = () => {
-    Modal.open({
-      visible: true,
-      title: "test",
-      content: "你好",
-      onCancel: () => { console.log("12") },
-      onConfirm: () => { console.log("confirm") }
-    })
+  const onClose = () => {
+    console.log("test");
   }
 
   return (
     <div className="App">
-      {/* <Button onClick={() => handleClick("open")}>open</Button>
-      <Button onClick={() => handleClick("info")}>info</Button>
-      <Button onClick={() => handleClick("success")}>success</Button>
-      <Button onClick={() => handleClick("error")}>error</Button>
-      <Button onClick={() => handleClick("warning")}>warning</Button> */}
-
-      {<Button onClick={modalClick}>open</Button>}
-      <Button onClick={handleModalClick}>open modal</Button>
-      <Modal
+      <Drawer
+        title={"Basic Drawer"}
+        placement="right"
+        closable={false}
+        onClose={onClose}
         visible={visible}
-        title={"this is modal title"}
-        onCancel={() => { setVisible(false) }}
-        onConfirm={() => { console.log("hello") }}
-        footer={null}
       >
         <p>Some contents...</p>
         <p>Some contents...</p>
-      </Modal>
-
-      <Affix>
-        <Button>affix</Button>
-      </Affix>
-      <Button style={{height: 1000}}>hahdsad</Button>
+        <p>Some contents...</p>
+      </Drawer>
     </div>
   );
 }
--- src/components/drawer/Drawer.tsx
+++ src/components/drawer/Drawer.tsx
@@ -1,10 +1,42 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
+import { judgeDOMExitAndCreateDOM } from "../../utils";
+import classnames from "classnames"
+import ReactDOM from "react-dom";
+import "./Drawer.scss"
 
-const Drawer = () => {
-    return (
-        <div>
-            drawer
-        </div>
+interface IDrawerProps {
+    title: string;
+    visible: boolean;
+    placement?: "top" | "right" | "bottom" | "left";
+    closable?:boolean;
+    onClose?:() => any;
+    style?: React.CSSProperties;
+    className?: string;
+}
+
+
+const Drawer: React.FC<IDrawerProps> = (props) => {
+    const drawerRef = useRef(judgeDOMExitAndCreateDOM("le-drawer-wrapper"));
+
+    const { style, className } = props
+
+    const classes = classnames("le-drawer", className, {
+
+    })
+
+    useEffect(() => {
+        return () => {
+            drawerRef.current.remove();
+        }
+    }, [])
+
+    return ReactDOM.createPortal(
+        <div className={classes} style={style}>
+            <div className="le-drawer-content">
+            
+            </div>
+        </div>,
+        drawerRef.current
     )
 }
 
