11204fbb62881789b8046c5f081e1b3e2e79906e
chenxiaole <2524729719@qq.com>
2020-11-19 17:59:44 +0800

    完善动画80%

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -9,15 +9,33 @@ interface ICarouselProps {
 
 const Carousel: React.FC<ICarouselProps> = memo(({ children, className, style }) => {
 
-  const [current, setCurrent] = useState(0)
+  const [current, setCurrent] = useState(1)
+
+  const [isExitTransition, setIsExitTransition] = useState(false)
 
   const leCarouselContainerRef = useRef<HTMLDivElement>(null)
+
+
+  useEffect(() => {
+    cloneNode()
+    goto(1)
+    return () => { setIsExitTransition(false) }
+  }, [])
+
+  useEffect(() => {
+    if (current === (children as any).length + 2) {
+      setIsExitTransition(false)
+      goto(2)
+    } else {
+      setIsExitTransition(true)
+    }
+  }, [current])
 
   const toRight = useCallback(() => {
     setCurrent(_ => {
       let next = _ + 1
-      if (next >= (children as any).length) {
-        next = 0
+      if (next >= (children as any).length + 2) {
+        next = 1
       }
       goto(next)
       return next
@@ -35,15 +53,29 @@ const Carousel: React.FC<ICarouselProps> = memo(({ children, className, style })
     })
   }, [])
 
+  const cloneNode = useCallback(() => {
+    const nodeList: HTMLElement[] = [];
+    (leCarouselContainerRef.current as HTMLDivElement).childNodes.forEach(node => {
+      if (node.nodeType === 1) {
+        const eleNode = node as HTMLElement
+        nodeList.push(eleNode)
+      }
+    });
+    (leCarouselContainerRef.current as HTMLDivElement).append(nodeList[0].cloneNode(true));
+    (leCarouselContainerRef.current as HTMLDivElement).prepend(nodeList[nodeList.length - 1].cloneNode(true));
+  }, [leCarouselContainerRef.current])
+
   const goto = useCallback((target: number) => {
     if (children) {
-      (leCarouselContainerRef.current as HTMLDivElement).style.transform = `translateX(${-(100 / (children as any).length) * target}%)`
+      (leCarouselContainerRef.current as HTMLDivElement).style.transform = `translateX(${-(100 / ((children as any).length + 2)) * target}%)`
     }
   }, [])
 
   return (
     <div className={classnames("le-carousel", className)} style={style}>
-      <div className="le-carousel-container" ref={leCarouselContainerRef}>
+      <div className={classnames("le-carousel-container", {
+        "isExitTransition": isExitTransition
+      })} ref={leCarouselContainerRef}>
         {React.Children.map(children, (_, i) => _)}
       </div>
       <span className="arrow_left" onClick={toLeft}>left</span>
