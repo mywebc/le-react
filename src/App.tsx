45329de255ef1a94b9b3272ecadf1a730fe963bd
chenxiaolani <2524729719@qq.com>
2020-12-06 15:37:50 +0800

    init input

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Affix from "./components/affix/Affix"
 import Drawer from "./components/drawer/Drawer"
 import { Layout, Header, Footer, Content, Side } from "./components/layout"
 import Carousel from "./components/carousel/Carousel"
+import Input from "./components/input/Input"
 
 type notificationType = "info" | "success" | "error" | "warning" | "open"
 
@@ -41,13 +42,26 @@ const App = () => {
       <Button onClick={handleNotification}>Notification</Button>
       <Button onClick={handleNotification1}>Notification</Button>
 
-      <Carousel style={{ border: "1px solid #aaa", width: 200 }} duration={1000} afterChange={(i) => { console.log("index", i) }}>
+      {/* <Carousel style={{ border: "1px solid #aaa", width: 200 }} duration={1000} afterChange={(i) => { console.log("index", i) }}>
         <div style={{ width: 200, height: 200, textAlign: "center", backgroundColor: "skyblue" }}>1</div>
         <div style={{ width: 200, height: 200, textAlign: "center", backgroundColor: "orange" }}>2</div>
         <div style={{ width: 200, height: 200, textAlign: "center", backgroundColor: "red" }}>3</div>
         <div style={{ width: 200, height: 200, textAlign: "center", backgroundColor: "black" }}>4</div>
-      </Carousel>
-    </div >
+      </Carousel> */}
+      <p>icon</p>
+      <Icon name={"txt"} />
+      <Icon name={"txt"}/>
+       <Icon name={"pin"}/>
+       <Icon name={"reeor"}/>
+       <Icon name={"close"}/>
+       <Icon name={"share"}/>
+       <Icon name={"sorting"} style={{fill: "#f56c6c"}}/>
+       <Icon name={"flag"} style={{fill: "#409eff"}}/>
+       <Icon name={"Top"} style={{fill: "#219c91"}}/>
+       <Icon name={"video"} style={{fill: "#67c23a"}}/>
+       <Icon name={"vs"} style={{fill: "skyblue"}}/>
+      <Input addonAfter=".com" addonBefore="http://"/>
+    </div>
   );
 }
 
--- src/components/input/Input.tsx
+++ src/components/input/Input.tsx
@@ -1,8 +1,28 @@
-import React from "react"
+import React, { memo, ReactNode } from "react"
+import classnames from "classnames"
+import "./Input.scss"
 
-
-const Input: React.FC = (props) => {
-    return <input type="text" placeholder="请输入值"/>
+interface IInputProps {
+    addonAfter?: ReactNode | string;
+    addonBefore?: ReactNode | string;
+    className?: string;
+    style?: React.CSSProperties;
 }
+
+const Input: React.FC<IInputProps> = memo(({ addonAfter, addonBefore, className, style }) => {
+
+
+    const classes = classnames("le-input-wrapper", className, {
+
+    })
+
+    return (
+        <div className={classes} style={style}>
+            {addonBefore && <span dangerouslySetInnerHTML={{ __html: addonBefore.toString() }}></span>}
+            <input className="le-input" type="text" placeholder="请输入值" />
+            {addonAfter && <span dangerouslySetInnerHTML={{ __html: addonAfter.toString() }}></span>}
+        </div>
+    )
+})
 
 export default Input;
